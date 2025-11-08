e8ba0d0d0b127696984d2ea5535ebc1788e62311
benmoshe <benmo@g.ariel.ac.il>
2025-11-08 18:55:18 +0200

    Ex1

--- src/assignments/Ex1/Ex1.java
+++ src/assignments/Ex1/Ex1.java
@@ -10,6 +10,7 @@ package assignments.Ex1;
  * This is the main Class you should implement (see "add your code below")
  *
  * @author boaz.benmoshe
+
  */
 public class Ex1 {
 	/** Epsilon value for numerical computation, it serves as a "close enough" threshold. */
