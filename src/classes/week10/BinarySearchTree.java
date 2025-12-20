a9b703d1a1358270a754bbeaf81e58409df521ec
benmoshe <benmo@g.ariel.ac.il>
2025-12-20 17:14:37 +0200

    Quiz suggested solution

---
+++ src/classes/week10/BinarySearchTree.java
@@ -0,0 +1,78 @@
+package classes.week10;
+/**
+ * 
+LLL:1
+LL:4
+LLRL:6
+LLR:6
+LLRR:7
+L:8
+:8
+RLL:9
+RL:9
+R:9
+
+                       8
+                 8           9
+            4             9
+         1     6        9
+             6   7
+
+ */
+import java.util.Comparator;
+
+public class BinarySearchTree<T> extends BinaryTree1<T> {
+
+	/**
+	 * 
+	 */
+	private Comparator<T> _comp;
+	public BinarySearchTree(Comparator<T> c) {super(null);_comp=c;}
+	//	public BinarySearchTree(Comparator<T> c) {this(c,null);}
+	public BinarySearchTree(Comparator<T> c,T a) {super(a);_comp=c;}
+
+	public Comparator<T> getComp() {
+		return _comp;
+	}
+	@Override
+	public void add(T a) {
+		// TODO Auto-generated method stub
+		if(this.isEmpty()) {_root = a;}
+		else {
+			//	double d = Math.random();
+			if(_comp.compare(a, _root)<=0) {
+				if(_left==null) {_left = new BinarySearchTree<T>(_comp,a);}
+				else {_left.add(a);}
+			}
+			else {
+				if(_right==null) {_right = new BinarySearchTree<T>(_comp,a);}
+				else {_right.add(a);}
+			}
+		}
+	}
+	@Override
+	public T find(T t) {
+		T ans = null;
+		if(!this.isEmpty()) {
+			int dir = _comp.compare(t,this.getRoot());
+			if(dir==0) {ans = getRoot();}
+			if(dir<0) {
+				BinaryTree<T> l = this.getLeft();
+				if(l!=null) {ans = l.find(t);}
+			}
+			if(dir>0) {
+				BinaryTree<T> r = this.getRight();
+				if(r!=null) {ans = r.find(t);}
+			}
+		}
+		return ans;
+	}
+	//@Override
+	/**
+	 * Removes the first element that equals to t.
+	 */
+	//public T remove(T t) {
+		///// try implementing this method - efficiently!
+	//	return null;
+	//}
+}
---
+++ src/classes/week10/TestingBT.java
@@ -0,0 +1,76 @@
+package classes.week10;
+
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class TestingBT {
+	public static void main(String[] args) {
+		int size = 1000;
+		BinaryTree<Integer> bt = new BinaryTree1<Integer>();
+		for(int i=0;i<size;i++) {
+			bt.add(i);
+		}
+		int h = BinaryTreeAlgo.height(bt);
+		System.out.println("Height: "+h);
+		Comparator<String> comp = new StringComparator();
+		BinarySearchTree<String> bst = new BinarySearchTree<String>(comp);
+		for(int i=0;i<size;i++) {
+			int e = (int)(Math.random()*1000);
+			bst.add("_"+e);
+		}
+		h = BinaryTreeAlgo.height(bst);
+		System.out.println("Height: "+h);
+		//	BinaryTreeAlgo.inOrderPrint(bst);	
+		String str = "saved_bst.obj";
+		BinaryTreeAlgo.save(bt, str);
+		BinaryTree<Integer> bt2 = (BinaryTree<Integer>)BinaryTreeAlgo.load(str);
+		System.out.println(bt.size()+" =?= "+bt2.size());
+		demoGUI();
+		
+		
+	}
+	/** Use StdDraw to draw a Binary Tree */
+	public static void demoGUI() {
+		Comparator<String> comp = new StringComparator();
+		BinarySearchTree<String> bst = new BinarySearchTree<String>(comp);
+		String[] nodes = {"5","2","0","4","9"}; 
+		for(int i=0;i<nodes.length;i++) {bst.add(nodes[i]);}
+		BinaryTreeAlgo.draw(bst);
+		Scanner sc = new Scanner(System.in);
+		String d="";
+		while(!d.equals("done")) {
+			System.out.println("input a String: (done to quit) ");
+			d = sc.next();
+			if(!d.equals("done")) {
+				bst.add(""+d);
+				BinaryTreeAlgo.draw(bst);
+				System.out.println(BinaryTreeAlgo.minLevelLeaf(bst));
+			}
+			
+		}
+		d="";
+		while(!d.equals("done")) {
+			System.out.println("input a String to delete: (done to quit) ");
+			d = sc.next();
+			if(!d.equals("done")) {
+				bst.remove(""+d);
+				BinaryTreeAlgo.draw(bst);
+			}
+		}
+	}
+}
+class StringComparator implements Comparator<String> {
+	public StringComparator(){;}
+	public int compare(String obj1, String obj2) {
+		if (obj1 == obj2) {
+			return 0;
+		}
+		if (obj1 == null) {
+			return -1;
+		}
+		if (obj2 == null) {
+			return 1;
+		}
+		return obj1.compareTo(obj2);
+	}
+}
